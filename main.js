037f2ac6ccd3ce4c09eaee4871f3410943edb567
Lipikaangajala <93141716+Lipikaangajala@users.noreply.github.com>
2023-05-22 20:44:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+  video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetetor = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+}
